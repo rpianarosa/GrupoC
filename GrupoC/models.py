9a416bbb0487d5fac8b40f5160abf347336a3934
rpianarosa <rama.pianaro@gmail.com>
2022-01-18 03:49:29 -0300

    Agregue cositas

--- GrupoC/models.py
+++ GrupoC/models.py
@@ -5,14 +5,26 @@ class Cerveza(models.Model):
     nombre=models.CharField('nombre', max_length=40)
     IBU=models.IntegerField()
     grad_alcohol=models.FloatField()
+
+    def __str__ (self):
+        return self.nombre
     
 class Cerveceria(models.Model):
     
     nombre=models.CharField('nombre', max_length=40)
     dire=models.CharField('dire', max_length=40)
+
+    def __str__ (self):
+        return self.nombre
 class Experiencia(models.Model):
     
     nombre=models.CharField('nombre', max_length=40)
     apellido=models.CharField('apellido', max_length=40)
-    exp=models.CharField('exp', max_length=50)
+    cerv_tomada=models.CharField('cervezaTomada', max_length=40)
+    cerv_atend=models.CharField('cervezaAtendida', max_length=40)
+    punt_cerveza=models.IntegerField()
+    punt_cerveceria=models.IntegerField()
+
+    def __str__ (self):
+        return self.nombre
     
--- GrupoC/urls.py
+++ GrupoC/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from GrupoC import views
+from GrupoC.views import CervezaList, CervezaCreate, CervezaDelete, CervezaUpdate, CervezaDetail
 
 urlpatterns = [
     path('', views.Inicio, name="Inicio"),
     path('Cervezas/', views.Cervezas, name="Cervezas"),
     path('Cervecerias/', views.Cervecerias, name="Cervecerias"),
-    path('Experiencias/', views.Experiencias, name="Experiencias")
+    path('Experiencias/', views.Experiencias, name="Experiencias"),
+
+    path('listaCervezas', views.CervezaList.as_view(),name='List'),
+    path('detalleCervezas/<pk>/', views.CervezaDetail.as_view(),name='Detail'),
+    path('editarCervezas/<pk>/', views.CervezaUpdate.as_view(),name='Edit'),
+    path('borrarCervezas/<pk>/', views.CervezaDelete.as_view(),name='Delete'),
+    path('nuevaCervezas', views.CervezaCreate.as_view(),name='New')
 ]
